63d7eace9e066fd2d402f14fc8467c363affb461
sonal-solaskar <solaskarsonal@gmail.com>
2024-03-31 21:05:57 +0530

    CPU FCFS, SJF, PRIORITY(NP & P)

---
+++ PriorityPCPU.py
@@ -0,0 +1,62 @@
+# PRIORITY PREEMPTIVE CPU SCHEDULING ALGORITHM
+
+def priority(plist):
+    t = 0
+    gantt = []
+    completed = {}
+    burst = {}
+    for p in plist:
+        burst[p[0]] = p[2]
+    print("BURST TIME:")
+    print(burst)
+    
+    while plist:
+        available = []
+        for p in plist:
+            if p[1] <= t:
+                available.append(p)
+                
+        if available == []:
+            gantt.append("IDLE")
+            t += 1
+            
+        else:
+            available.sort(key=lambda x: x[3])
+            process = available[0]
+            gantt.append(process[0])
+            t += 1 
+            plist.remove(process)
+            process[2] -= 1
+            
+            if process[2] == 0:  #completed process
+                pid = process[0]
+                completed[pid] = t
+                at = process[1]
+                bt = burst[pid]
+                ct = t
+                tat = ct - at
+                wt = tat - bt
+                completed[pid] = [ct, tat, wt]
+                
+            else:
+                plist.append(process)
+    
+    print("Gantt Chart: ")
+    print(gantt)
+    print("Completed: ")  
+    print(completed)            
+            
+            
+
+if __name__ == "__main__":
+    plist = []
+    n = int(input("Enter the number of Processes: "))
+    
+    for i in range(0,n):
+        ele = input(f"Enter PID, AT, BT and Priority for Process{i+1}: ")
+        elelist = ele.split()
+        elelist[1] = int(elelist[1])
+        elelist[2] = int(elelist[2])
+        elelist[3] = int(elelist[3])
+        plist.append(elelist)
+    priority(plist)
